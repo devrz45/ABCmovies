a94977320cb070f34f3dfc1f603c98c67f0b0dac
Devendra Parkar <devendra.parkar@games24x7.com>
2020-04-11 16:48:07 +0530

    removed a url bug

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production"
 
 export const server = dev
 	? "http://localhost:3000"
-	: "https://abcmovies.herokuapp.com/"
+	: "https://abcmovies.herokuapp.com"
