80d98048540ecf169b11cdd9e8fe2642380a7446
Devendra Parkar <devendra.parkar@games24x7.com>
2020-04-13 12:45:08 +0530

    enabled cors

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,11 +1,46 @@
 import fetch from "isomorphic-unfetch"
 import { searchData, SearchData2, ErrorResponse, movieData } from "./mock/data"
+import Cors from "cors"
 
 const API_KEY = "ce90f7c6"
 
+// Initializing the cors middleware
+const whitelist = [
+	"https://abcmovies.herokuapp.com",
+	"http://abcmovies.herokuapp.com",
+	"http://localhost:3000",
+]
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			callback(null, true)
+		} else {
+			callback(new Error("Not allowed by CORS"))
+		}
+	},
+	optionsSuccessStatus: 200,
+}
+const cors = Cors(corsOptions)
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result) => {
+			if (result instanceof Error) {
+				return reject(result)
+			}
+
+			return resolve(result)
+		})
+	})
+}
+
 export default async (req, res) => {
+	// Run the middleware
+	await runMiddleware(req, res, cors)
+
 	let params = req.query
-	// let data = null
 	if (!params)
 		return res.status(400).json({
 			Response: "False",
@@ -17,8 +52,10 @@ export default async (req, res) => {
 	let url = getURL(params)
 	const data = await fetch(url).then((response) => response.json())
 	/* Using mocks */
+	// let data = null
 	// if (params.i) data = movieData
 	// else data = params.page % 2 == 0 ? searchData : SearchData2
+
 	res.status(200).json(data)
 }
 
